30ce6718d7b6f659ae0c8a44fe21e3e8cbbb12dd
nyakako <nyakako13@gmail.com>
2024-05-29 16:03:17 +0900

    feat: #2 wip

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -6,8 +6,10 @@
 		public github_id: string,
 		public qiita_id: string,
 		public x_id: string,
-		public skills: string
+		public skills: string[]
 	) {
-		// this. = ();
+		this.github_id = `https://github.com/${github_id}`;
+		this.qiita_id = `https://qiita.com/${qiita_id}`;
+		this.x_id = `https://x.com/${x_id}`;
 	}
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
-import { Card } from "./components/Card.tsx";
+import { BussinessCard } from "./components/BussinessCard.tsx";
+import theme from "./theme/theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<App />} />
-					<Route path="cards/:id" element={<Card />} />
+					<Route path="cards/:id" element={<BussinessCard />} />
 				</Routes>
 			</BrowserRouter>
 		</ChakraProvider>
