e7e387520ac4ac0f2ec33b12892ae84a5e66660e
nyakako <nyakako13@gmail.com>
2024-05-31 09:07:34 +0900

    fix sanitize

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
+import SanitizedComponent from "./components/SanitizedComponent";
+
 function App() {
-	// const testInputs = [
-	// 	"<script>alert('XSS');</script>",
-	// 	'<img src="x" onerror="alert(\'XSS\')" />',
-	// 	"<a href=\"javascript:alert('XSS')\">Click me</a>",
-	// 	"<div style=\"background: url(javascript:alert('XSS'))\">Test</div>",
-	// 	"<<script>alert('XSS');//</script>",
-	// ];
+	const testInputs = [
+		"<script>alert('XSS');</script>",
+		'<img src="x" onerror="alert(\'XSS\')" />',
+		"<a href=\"javascript:alert('XSS')\">Click me</a>",
+		"<div style=\"background: url(javascript:alert('XSS'))\">Test</div>",
+		"<<script>alert('XSS');//</script>",
+	];
 
 	return (
 		<>
 			<h1>Hello World</h1>
 			<h2>Sanitization Test</h2>
-			{/* {testInputs.map((input, index) => (
+			{testInputs.map((input, index) => (
 				<div key={index}>
 					<h3>Test {index + 1}</h3>
+					{input}
 					<SanitizedComponent html={input} />
 				</div>
-			))} */}
+			))}
 		</>
 	);
 }
