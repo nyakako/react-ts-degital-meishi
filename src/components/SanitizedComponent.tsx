d9140cef2a169853bbd224cf281915fbfcf85663
nyakako <nyakako13@gmail.com>
2024-06-17 09:54:17 +0900

    fix

--- src/components/SanitizedComponent.tsx
+++ src/components/SanitizedComponent.tsx
@@ -7,9 +7,10 @@ interface Props {
 
 const SanitizedComponent: FC<Props> = ({ html }) => {
 	// const sanitizedHtml = DOMPurify.sanitize(html);
-
 	// return parse(sanitizedHtml);
+
 	return parse(html);
+
 	// 比較用
 	// return <div dangerouslySetInnerHTML={{ __html: sanitizedHtml }}></div>;
 };
